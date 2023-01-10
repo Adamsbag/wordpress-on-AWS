e0ac604d3fdc14cac1275a431abeb5593ff28983
Adamsbag <79222610+Adamsbag@users.noreply.github.com>
2023-01-10 11:52:31 -0500

    Add S3 and Dynamodb for remote state file

---
+++ remote_backend/dynamodb.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "terraform_remote_state" {
+  bucket = "wordpress-aws-adamab"
+}
+
+resource "aws_dynamodb_table" "terraform_remote_state" {
+  name           = "wordpress-remote-state-db"
+  billing_mode   = "PROVISIONED"
+  read_capacity  = 1
+  write_capacity = 1
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+}
