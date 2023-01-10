155a870a239118571a120a27ee48c224af97234a
Adamsbag <79222610+Adamsbag@users.noreply.github.com>
2023-01-10 12:54:20 -0500

    Update README.md file

--- remote_backend/dynamodb.tf
+++ remote_backend/dynamodb.tf
@@ -1,7 +1,9 @@
+# Create the S3 Bucket for the remote states storage
 resource "aws_s3_bucket" "terraform_remote_state" {
   bucket = "wordpress-aws-adamab"
 }
 
+# Create a Dynamodb table to Lock the state file
 resource "aws_dynamodb_table" "terraform_remote_state" {
   name           = "wordpress-remote-state-db"
   billing_mode   = "PROVISIONED"
