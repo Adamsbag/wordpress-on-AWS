9a10fee8d83b974dfddc68e1c51a28fe974e1aa7
Adamsbag <79222610+Adamsbag@users.noreply.github.com>
2023-01-29 03:57:41 -0500

    RDS Connection update

--- backend/user-data.sh
+++ backend/user-data.sh
@@ -1,46 +1,51 @@
 #!/bin/bash
 
-# import RDS credentials from AWS Secrets Manager
+# Update the system
+sudo yum update -y
+
+# Install Apache
+sudo yum install httpd mariadb-server mariadb -y
+
+# Install PHP
+sudo amazon-linux-extras install php8.1
+
+# Start Apache and Mariadb
+sudo systemctl start httpd
+sudo systemctl start mariadb
+
+# Enable Apache and Mariadb to start on boot
+sudo systemctl enable httpd
+sudo systemctl enable mariadb
+
+# Import RDS credentials from AWS Secrets Manager
 secret=$(aws secretsmanager get-secret-value --secret-id "main/rds/password")
 username=$(echo $secret | jq -r .SecretString | jq -r .username)
 password=$(echo $secret | jq -r .SecretString | jq -r .password)
-endpoint=$(echo $secret | jq -r .SecretString | jq -r .endpoint)
+endpoint=$(terraform output rds_endpoint)
 
-# update the system
-sudo yum update -y
-
-# install Apache and PHP
-sudo yum install httpd php php-mysql -y
-
-# start Apache
-sudo systemctl start httpd
-
-# enable Apache to start on boot
-sudo systemctl enable httpd
-
-# download the latest version of WordPress
+# Download the latest version of WordPress
 curl -O https://wordpress.org/latest.tar.gz
 
-# extract the downloaded file
+# Extract the downloaded file
 tar xzvf latest.tar.gz
 
-# move the files to the Apache document root
+# Move the files to the Apache document root
 sudo mv wordpress/* /var/www/html
 
-# set the correct permissions on the WordPress files
+# Set the correct permissions on the WordPress files
 sudo chown -R apache:apache /var/www/html
 
-# navigate to the WordPress directory
+# Navigate to the WordPress directory
 cd /var/www/html
 
-# copy the sample configuration file
+# Copy the sample configuration file
 cp wp-config-sample.php wp-config.php
 
-# update the configuration with the RDS endpoint and credentials
+# Update the configuration with the RDS endpoint and credentials
 sed -i "s/database_name_here/mydb/g" wp-config.php
 sed -i "s/username_here/$username/g" wp-config.php
 sed -i "s/password_here/$password/g" wp-config.php
 sed -i "s/localhost/$endpoint/g" wp-config.php
 
-# restart Apache
+# Restart Apache
 sudo systemctl restart httpd
